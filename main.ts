ed2554884bb896b10365507fe5a1c43f0e123836
Angelica-Riego-OCreen <angelica.ocreen@stu.ocsb.ca>
2026-03-23 13:29:14 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,38 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// setup
+let AmountOfLight:number = 0
+let neopixelStrip: neopixel.Strip = null 
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+
+//Press A
+input.onButtonPressed(Button.A, function() {
+  basic.clearScreen()
+  input.lightLevel()
+  if (AmountOfLight <= 51) {
+    neopixelStrip.clear()
+}
+
+if (AmountOfLight > 52) {
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+}
+
+if (AmountOfLight > 104) {
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+}
+
+if (AmountOfLight > 156) {
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+}
+
+if (AmountOfLight > 208) {
+    neopixelStrip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
+}
+  
+})
+
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  
